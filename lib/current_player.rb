ad2ba6c1dbf33c3d4904c2e0ded2155e7af994ac
york1670@gmail.com <york1670@gmail.com>
2018-08-29 05:46:23 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,9 +3,8 @@ a_board = [" "," "," "," "," "," "," "," "," "]
 def turn_count(board)
   # iterate over each element in the board
   # if that element is "O" or "X" add one to the turns_played counter
-  turns_played = 0
-
   board.each do |elements|
+    turns_played = 0
     if elements != " " || square != "" || square != nil
       then turns_played += 1
     end
