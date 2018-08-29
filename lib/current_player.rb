a9181013b3a1df9d2e9aa579c2700bad9f8855a0
york1670@gmail.com <york1670@gmail.com>
2018-08-29 04:49:26 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,18 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def turn_count
+  unoccupied_position = board.size 
+  occupied_position = 0 
+
+  while unoccupied_position != 0
+    occupied_position += 1
+  end
 end
+
 def current_player
+  if turn_count % 2
+    puts "O"
+  else
+    puts "X"
+  end
 end
