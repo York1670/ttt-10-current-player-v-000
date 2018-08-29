74d30b42d095384af69e4be63525cf57eecc35ea
york1670@gmail.com <york1670@gmail.com>
2018-08-29 05:14:12 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,16 +1,20 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def turn_count
-  turns_available = board.size
+  turns_available = 9
   turns_played = 0
   while turns_played < turns_available
-    puts "turns played #{turns_played}"
     turns_played +=1
+    if turns_played % 2
+      puts "O"
+    else
+      puts "X"
+    end
   end
 end
 
 def current_player
-  if turn_count % 2
+  if turn_count(turns_played) % 2
     puts "O"
   else
     puts "X"
